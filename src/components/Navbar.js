486e6c143ef985a52dfe8f401c1c977879726537
Kochapas Santibut <kochapas@outlook.com>
2023-03-06 21:58:02 +0900

    Add navbar placeholder, update CSS.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,30 @@
+import Image from "next/image";
+import styles from '@/styles/Navbar.module.css'
+
 export const Navbar = ({children}) => {
-  console.log("Navbar is loaded. ==>");
+
   return (
     <>
-      <div>
-        {children}
+      <div className={styles.navbar}>
+        <div className="container d-flex justify-content-between">
+          <div className={styles.navbar__avatar}>
+            <Image
+              src="/assets/parrot.jpg"
+              alt="Navbar avatar"
+              width="32"
+              height="32"
+              className="rounded-circle"
+            />
+          </div>
+          <div className={styles.navbar__info}>
+            <p className="m-auto me-2">X</p>
+            <p className="m-auto me-2">X</p>
+            <p className="m-auto me-2">X</p>
+            <p className="m-auto me-2">X</p>
+          </div>
+        </div>
       </div>
+      {children}
     </>
   );
 }
