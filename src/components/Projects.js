ff4e8620a61a2eed01d0e77bbdfad2d68ad530f0
Kochapas Santibut <kochapas@outlook.com>
2023-03-14 01:02:38 +0900

    Move projects data to translation file. Add translation to Projects component.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,60 +5,21 @@ import { useWindowSize } from "@/hooks";
 import styles from "@/styles/Projects.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { useTranslation } from "react-i18next";
 
 export const Projects = () => {
+  const { t } = useTranslation();
   const windowSize = useWindowSize();
   const [expanded, setExpanded] = useState(false);
 
-  // TODO: Replace this with the data from API.
   const DEFAULT_IMG_SRC ="/assets/projects/default.jpg";
-  const projects = [
-    {
-      name: "Go SubscRide",
-      primaryType: "Shopify Application",
-      secondaryType: "Production",
-      description: "An application to help the merchant to manage customer's subscription contracts. Running on Shopify platform. I was working closely on this project to implement new features and maintaining the app while I was working with GO RIDE.",
-      roles: ["Fullstack Developer", "Team Member"],
-      stacks: ["Node", "React", "GraphQL", "PostgresQL"],
-      imgSrc: "/assets/projects/gosub.png",
-      link: "https://apps.shopify.com/go-subscride",
-    },
-    {
-      name: "Stripe Migration Application",
-      primaryType: "Shopify Application",
-      secondaryType: "Production",
-      description: "An application to help merchant to migrate their customers' payment information from Stripe platform to Shopify platform. I create this custom application to help my team member from doing repetitive task during the store's migration process.",
-      roles: ["Fullstack Developer", "Solo Member"],
-      stacks: ["Node", "React"],
-      imgSrc: "/assets/projects/stripe.png",
-    },
-    {
-      name: "mamoru.earth",
-      primaryType: "Web Application",
-      secondaryType: "Production",
-      description: "An application that helps create sustainability and connect the business to their target consumer. I voluntarily work with them to make the world a better place for future generations.",
-      roles: ["Frontend Developer", "Team Member"],
-      stacks: ["React"],
-      imgSrc: "/assets/projects/mamoru.png",
-      link: "https://app.mamoru.earth"
-    },
-    {
-      name: "BarcSnap",
-      primaryType: "Web Application",
-      secondaryType: "Side Project / Discontinued",
-      description: "An application I creates with my teammate during coding bootcamp. The goals of the application is to help foreigers who travel or lives in the other country where they can't read the local language. BarcSnap helping them by telling the detailed information of the product by just scanning the barcode without being able to read the product's name.",
-      roles: ["Fullstack Developer", "Team Leader"],
-      stacks: ["Ruby on Rails", "PostgresQL"],
-      imgSrc: "/assets/projects/barcsnap.png",
-      link: "https://github.com/2tonepantone/barcsnap"
-    },
-  ]
+  const projects = ["goSub", "stripe", "mamoru", "barcSnap"];
 
   if (!expanded) {
     return (
     <div className="w-100 text-center mt-3">
       <Button onClick={() => setExpanded(true)}>
-        {"Discover my projects"}
+        {t(`Project.cta`)}
       </Button>
     </div>
   );
@@ -67,20 +28,18 @@ export const Projects = () => {
   return (
     <div className={`${windowSize.containerClass} ${expanded ? styles["projects-container--show"] : styles["projects-container--hide"]} mt-3`}>
       <div className="w-100 border-bottom border-secondary">
-        <h2 className="text--purple">{"PROJECTS"}</h2>
+        <h2 className="text--purple">{t(`Project.title`)}</h2>
       </div>
       <div className="row">
         {projects.map((project, index) => {
-          const {
-            name,
-            primaryType,
-            secondaryType,
-            description,
-            roles,
-            stacks,
-            imgSrc,
-            link,
-          } = project;
+          const name = t(`Project.${project}.name`);
+          const primaryType = t(`Project.${project}.primaryType`);
+          const secondaryType = t(`Project.${project}.secondaryType`);
+          const description = t(`Project.${project}.description`);
+          const roles = t(`Project.${project}.roles`)?.split(",") || [];
+          const stacks = t(`Project.${project}.stacks`)?.split(",") || [];
+          const imgSrc = t(`Project.${project}.imgSrc`);
+          const url = t(`Project.${project}.url`);
 
           return (
             <div key={`project-${index}`} className="col-12 col-lg-6">
@@ -93,9 +52,9 @@ export const Projects = () => {
                   className="w-100 h-auto pt-2 mb-3"
                 />
                 <div>
-                  {(!!link) ?
+                  {(!!url) ?
                     <a
-                      href={link}
+                      href={url}
                       target="_blank"
                       rel="noopener noreferrer"
                       className="d-flex text-decoration-none align-items-center"
