47412bdadede8ca8d24b69e60d446aee89b66abd
Kochapas Santibut <kochapas@outlook.com>
2023-03-14 17:21:25 +0900

    Add nextjs google analytics.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ import {
   FloatMenu
 } from "@/components";
 import "@/translations/i18n";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 // Since fontawesome css is imported above. Stop it from importing again.
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
@@ -13,6 +14,7 @@ config.autoAddCss = false;
 const MyProviders = ({ children }) => {
   return (
     <>
+      <GoogleAnalytics trackPageViews />
       <Navbar>
         <FloatMenu>
           {children}
