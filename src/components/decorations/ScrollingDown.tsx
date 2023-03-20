ca936b3bc70df7d34869b4e11aea6f88a0277f9e
Kochapas Santibut <kochapas@outlook.com>
2023-03-20 20:56:24 +0900

    Convert ScrollingDown to TypeScript.

--- src/components/decorations/ScrollingDown.tsx
+++ src/components/decorations/ScrollingDown.tsx
@@ -1,6 +1,13 @@
+import { ReactNode } from "react";
 import styles from "@/styles/ScrollingDown.module.css";
 
-export const ScrollingDown = ({ height, className, children }) => {
+type Props = {
+  height: string | number,
+  className: string,
+  children: ReactNode,
+}
+
+export const ScrollingDown = ({ height, className, children }: Props) => {
   return (
     <div
       className={`w-auto d-flex justify-content-center mt-3 mb-3 ${className}`}
